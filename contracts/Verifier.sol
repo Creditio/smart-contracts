189cece27f77df8a43cb9cf2fe92dc3eff84c2b0
Parshwa Shah <parshwa.surat@gmail.com>
2022-12-04 05:41:30 +0530

    Added verifier and code for signature verification

--- contracts/Verifier.sol
+++ contracts/Verifier.sol
@@ -7,11 +7,14 @@ import "./interfaces/ICircuitValidator.sol";
 import "./verifiers/ZKPVerifier.sol";
 
 contract Verifier is ZKPVerifier {
-
     uint64 public constant TRANSFER_REQUEST_ID = 1;
 
-    mapping(address => uint256) ids;
-    mapping(string => uint256) reputation;
+    mapping(address => bool) allowed;
+    mapping(uint256 => uint256) reputation;
+    mapping(uint256 => uint256) timestamp;
+    mapping(address => uint128) ids;
+    address private _issuerAddress;
+    uint256 public noOfUsers = 0;
 
     function _beforeProofSubmit(
         uint64, /* requestId */
@@ -28,22 +31,57 @@ contract Verifier is ZKPVerifier {
         );
     }
 
+    modifier verify(
+        bytes memory signature,
+        uint256 application,
+        uint256 _amount
+    ) {
+        bytes32 digest = keccak256(abi.encode(application, _amount));
+        digest = digest.toEthSignedMessageHash();
+        address signer = ECDSA.recover(digest, signature);
+        require(signer == ownerAddress, "Invalid signature");
+        require(block.timestamp <= _deadline, "Signature expired");
+        require(_user == msg.sender, "Invalid user");
+        _;
+    }
+
+    function setIssuerAddress(address _issuerAddress) public onlyAdmin {
+        issuerAddress = _issuerAddress;
+    }
+
     function _afterProofSubmit(
         uint64 requestId,
         uint256[] memory inputs,
         ICircuitValidator validator
     ) internal override {
         require(
-            requestId == TRANSFER_REQUEST_ID && addressToId[_msgSender()] == 0,
+            requestId == TRANSFER_REQUEST_ID,
             "proof can not be submitted more than once"
         );
 
         uint256 id = inputs[validator.getChallengeInputIndex()];
-        // execute the airdrop
-        if (idToAddress[id] == address(0)) {
-            super._mint(_msgSender(), TOKEN_AMOUNT_FOR_AIRDROP_PER_ID);
-            addressToId[_msgSender()] = id;
-            idToAddress[id] = _msgSender();
+
+        if (allowed[_msgSender()] == 0) {
+            allowed[_msgSender()] = 1;
         }
+    }
+
+    function verifyAndUpdateReputation(
+        uint256 application,
+        uint256 _amount,
+        bytes memory signature
+    ) external verify(signature, application, _amount) {
+        require(
+            allowed[_msgSender()] == 1,
+            "You're not allowed to access this function"
+        );
+        allowed[_msgSender()] = 0;
+        if (ids[_msgSender()] == 0) {
+            ids[_msgSender()] = noOfUsers;
+            noOfUsers++;
+        }
+        reputation[ids[_msgSender()] * 10 + application] = _amount;
+        timestamp[reputation[ids[_msgSender()] * 10 + application]] = block
+            .timestamp;
     }
 }
